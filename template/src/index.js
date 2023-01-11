7748b86dd4780f526e135add9a2cce70c03f60c2
Scynes <dustin.greyfield@gmail.com>
2023-01-10 20:03:10 -0500

    added basic hello world component props example and refactroed redundant code

--- template/src/index.js
+++ template/src/index.js
@@ -1,33 +1,19 @@
-/**
- * Import the React JavaScript library.
- */
 import React from 'react';
 
-/**
- * Import the react-dom module that handles rendering for React
- */
 import ReactDOM from 'react-dom/client';
 
-/**
- * Import the application entry point.
- */
-import Application from './Application';
+import App from './App';
 
 import './assets/index.css'
 
-/**
- * Define the root element for react-dom.
- */
 const ROOT = ReactDOM.createRoot(document.getElementById('root'));
 
-/**
- * Build the application on start.
- */
+// Build the application with initial logic (if needed)
 const build = () => {
 
     ROOT.render(
         <React.StrictMode>
-            <Application />
+            <App />
         </React.StrictMode>
     );
 }
